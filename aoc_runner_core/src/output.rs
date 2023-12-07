cd5f9467bb254519753daec0e719638f515c8d8b
Sam Noedel <sam.noedel@gmail.com>
2023-12-06 21:16:46 -0800

    Implement SolutionOutput for more types

--- aoc_runner_core/src/output.rs
+++ aoc_runner_core/src/output.rs
@@ -5,20 +5,17 @@ pub trait SolutionOutput {
     fn to_string(&self) -> Result<String>;
 }
 
-impl SolutionOutput for String {
-    fn to_string(&self) -> Result<String> {
-        Ok(ToString::to_string(self))
-    }
+/// Easily implement `SolutionOutput` for types which implement `ToString`
+macro_rules! output_str {
+    ($($type:ty),+) => {
+        $(
+            impl SolutionOutput for $type {
+                fn to_string(&self) -> Result<String> {
+                    Ok(ToString::to_string(self))
+                }
+            }
+        )+
+    };
 }
 
-impl SolutionOutput for u32 {
-    fn to_string(&self) -> Result<String> {
-        Ok(ToString::to_string(self))
-    }
-}
-
-impl SolutionOutput for usize {
-    fn to_string(&self) -> Result<String> {
-        Ok(ToString::to_string(self))
-    }
-}
+output_str![&str, String, u8, i8, u16, i16, u32, i32, u64, i64, u128, i128, usize, isize];
