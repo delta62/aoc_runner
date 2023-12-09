458d30e6ec539b7e344bf4ab4d240c6c85ef106a
Sam Noedel <sam.noedel@gmail.com>
2023-12-08 16:11:36 -0800

    Make results output-able

--- aoc_runner_core/src/output.rs
+++ aoc_runner_core/src/output.rs
@@ -2,7 +2,7 @@ use crate::error::Result;
 
 /// A value which can be returned from an Advent of Code solver function
 pub trait SolutionOutput {
-    fn to_string(&self) -> Result<String>;
+    fn to_string(self) -> Result<String>;
 }
 
 /// Easily implement `SolutionOutput` for types which implement `ToString`
@@ -10,8 +10,8 @@ macro_rules! output_str {
     ($($type:ty),+) => {
         $(
             impl SolutionOutput for $type {
-                fn to_string(&self) -> Result<String> {
-                    Ok(ToString::to_string(self))
+                fn to_string(self) -> Result<String> {
+                    Ok(ToString::to_string(&self))
                 }
             }
         )+
@@ -19,3 +19,12 @@ macro_rules! output_str {
 }
 
 output_str![&str, String, u8, i8, u16, i16, u32, i32, u64, i64, u128, i128, usize, isize];
+
+impl<T> SolutionOutput for Result<T>
+where
+    T: ToString,
+{
+    fn to_string(self) -> Result<String> {
+        self.map(|x| x.to_string())
+    }
+}
